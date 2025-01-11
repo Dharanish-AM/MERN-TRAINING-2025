989c81bd48371182c495fba72049804cf18b83f1
Dharanish A M <dharanish816@gmail.com>
2025-01-11 21:50:18 +0530

    Async Callback - JS

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-var a = [1,"two","three"]
-console.log(a)
+var a = [1, "two", "three"];
+console.log(a);
 
-var b = [{
-    "name" : "jp",
-    "class" : "CSE A"
-},
-{
-    "name" : "aji",
-    "class" : "CSE A"
-},
-{
-    "name" : "Dharanish",
-    "class" : "CSE A"
-}]
+var b = [
+  {
+    name: "jp",
+    class: "CSE A",
+  },
+  {
+    name: "aji",
+    class: "CSE A",
+  },
+  {
+    name: "Dharanish",
+    class: "CSE A",
+  },
+];
 b.forEach((person) => {
-    console.log(person.name,person.class)
-})
+  console.log(person.name, person.class);
+});
+
+for (let [i, j] of b.entries()) {
+  console.log(i, j);
+}
+
+const arr = [10, 20, 40, 50];
+
+let brr = [...arr, 60];
+let crr = [60,...arr]
+
+const func = ()=>{
+    console.log("Hello");
+}
+
+console.log("A :", arr);
+console.log("B :", brr);
+console.log("C :", crr);
+func()
