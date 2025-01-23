fcbef46e46f6ea9c72398b35087be2631e34d72b
Dharanish A M <dharanish816@gmail.com>
2025-01-23 18:45:40 +0530

    minor changes 2

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import Navbar from './components/Navbar.jsx';
+import Navbar from './components/Nav';
 import './App.css';
 import Home from './components/Home';
 import About from './components/About';
